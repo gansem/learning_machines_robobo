d99504926b93473df7c9f79802ef0ec4aa318fa5
Etienne Galea <egalea.11@gmail.com>
2021-01-11 13:44:32 +0100

    establishing validation fn

--- src/send_commands.py
+++ src/send_commands.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 from __future__ import print_function
 
 import time
@@ -10,7 +10,6 @@ import sys
 import signal
 import prey
 
-
 def terminate_program(signal_number, frame):
     print("Ctrl-C received, terminating program")
     sys.exit(1)
@@ -19,41 +18,50 @@ def main():
     signal.signal(signal.SIGINT, terminate_program)
 
     # rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
-    rob = robobo.SimulationRobobo().connect(address='192.168.1.6', port=19997)
+    rob = robobo.SimulationRobobo().connect(address='127.0.0.1', port=19997)
 
     rob.play_simulation()
 
-    # Following code moves the robot
-    for i in range(10):
-            print("robobo is at {}".format(rob.position()))
-            rob.move(5, 5, 2000)
+    # speed of wheels (+ direction)
+    l_wheel = 25
+    r_wheel = 25
+    # time spent moving (ms)
+    t_move = 500
+
+    # actual measure to check rewards against (max value)
+    v_measure = 0
+
+    # Following code moves the robot (forever)
+    while True:
+        # print("robobo is at {}".format(rob.position()))
+        rob.move(l_wheel, r_wheel, t_move)
+        print(f"ROB Irs: {rob.read_irs()}")
+
+        # if any IRS detects an object, add to validity measure
+        if any(rob.read_irs()):
+            v_measure += np.mean([l_wheel, r_wheel]) * t_move
+
+        
    
     print("robobo is at {}".format(rob.position()))
     rob.sleep(1)
 
     # Following code moves the phone stand
-    rob.set_phone_pan(343, 100)
-    rob.set_phone_tilt(109, 100)
-    time.sleep(1)
-    rob.set_phone_pan(11, 100)
-    rob.set_phone_tilt(26, 100)
-
-    # Following code makes the robot talk and be emotional
-    rob.set_emotion('happy')
-    rob.talk('Hi, my name is Robobo')
-    rob.sleep(1)
-    rob.set_emotion('sad')
+    # rob.set_phone_pan(343, 100)
+    # rob.set_phone_tilt(109, 100)
+    # rob.set_phone_pan(11, 100)
+    # rob.set_phone_tilt(26, 100)
 
     # Following code gets an image from the camera
-    image = rob.get_image_front()
-    cv2.imwrite("test_pictures.png",image)
+    # image = rob.get_image_front()
+    # cv2.imwrite("test_pictures.png",image)
 
-    time.sleep(0.1)
+    # time.sleep(0.1)
 
     # IR reading
-    for i in range(1000000):
-        print("ROB Irs: {}".format(np.log(np.array(rob.read_irs()))/10))
-        time.sleep(0.1)
+    # for i in range(100):
+    #     print("ROB Irs: {}".format(np.log(np.array(rob.read_irs()))/10))
+    #     time.sleep(0.1)
 
     # pause the simulation and read the collected food
     rob.pause_simulation()
