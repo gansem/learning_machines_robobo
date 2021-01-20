56037bcf2eaec3d0c6bcb94d74ffbc2cd1eec22f
Etienne Galea <egalea.11@gmail.com>
2021-01-20 16:12:11 +0100

    Added episode counter and restart

--- src/dumb_robobo.py
+++ src/dumb_robobo.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import pandas as pd
 import math
+import random
 import info
 
 import robobo
@@ -12,7 +13,7 @@ import cv2
 import sys
 import signal
 import prey
-
+import vrep
 
 def terminate_program(signal_number, frame):
     print("Ctrl-C received, terminating program")
@@ -79,8 +80,16 @@ def decide_cam_move_reduced_actions(cam_obs, actions):
 
     # find mid in observation list
     mid_index = math.floor(len(cam_obs) / 2)
-    # find the most food in image section
-    max_index = cam_obs.index(max(cam_obs))
+    # find the most food in image section (middle has increased weighting)
+    obs_values = []
+    for i, v in enumerate(cam_obs):
+        value = v
+        if mid_index == i:
+            value = v*1.5
+        obs_values.append(value)
+    max_index = obs_values.index(max(obs_values))
+
+    print(f"observations values:\t{obs_values[0]}\t{obs_values[1]}\t{obs_values[2]}")
 
     # if food is in the middle, go straight forward
     if max_index == mid_index:
@@ -142,7 +151,7 @@ def main(task):
     rob = robobo.SimulationRobobo().connect(address='127.0.0.1', port=19997)
     rob.play_simulation()
     # tilt robot camera a bit downwards
-    rob.set_phone_tilt(np.pi / 7.0, 10)
+    rob.set_phone_tilt(np.pi / 4.0, 10)
 
     # import actions
     actions = info.actions
@@ -172,13 +181,53 @@ def parse_irs(irs_values):
 def calc_distance_travelled(start_pos, stop_pos):
     return math.sqrt((stop_pos[0] - start_pos[0])**2 + (stop_pos[1] - start_pos[1])**2)
 
+def _reset_food(rob, food_names):
+    # collects positions of the walls (only works on standard map)
+    leftwall_handle = vrep.unwrap_vrep(vrep.simxGetObjectHandle(rob._clientID, '80cmHighWall200cm', vrep.simx_opmode_blocking))
+    pos_lw = vrep.unwrap_vrep(vrep.simxGetObjectPosition(rob._clientID, leftwall_handle, -1, vrep.simx_opmode_blocking))
+    
+    rightwall_handle = vrep.unwrap_vrep(vrep.simxGetObjectHandle(rob._clientID, '80cmHighWall200cm1', vrep.simx_opmode_blocking))
+    pos_rw = vrep.unwrap_vrep(vrep.simxGetObjectPosition(rob._clientID, rightwall_handle, -1, vrep.simx_opmode_blocking))
+    
+    bottomwall_handle = vrep.unwrap_vrep(vrep.simxGetObjectHandle(rob._clientID, '80cmHighWall200cm2', vrep.simx_opmode_blocking))
+    pos_bw = vrep.unwrap_vrep(vrep.simxGetObjectPosition(rob._clientID, bottomwall_handle, -1, vrep.simx_opmode_blocking))
+    
+    topwall_handle = vrep.unwrap_vrep(vrep.simxGetObjectHandle(rob._clientID, '80cmHighWall200cm0', vrep.simx_opmode_blocking))
+    pos_tw = vrep.unwrap_vrep(vrep.simxGetObjectPosition(rob._clientID, topwall_handle, -1, vrep.simx_opmode_blocking))
+    
+    food_pos = []
+    for food in food_names:
+        food_handle = vrep.unwrap_vrep(vrep.simxGetObjectHandle(rob._clientID, food, vrep.simx_opmode_blocking))
+        new_pos = vrep.unwrap_vrep(vrep.simxGetObjectPosition(rob._clientID, food_handle, -1, vrep.simx_opmode_blocking))
+        if new_pos[2] - 1 > 0:
+            new_pos[2] -= 1  # reset height of food
+        # from provided script on canvas
+        new_pos[0] = (random.uniform((pos_lw[0] + 0.25), (pos_rw[0] - 0.25)))
+        new_pos[1] = (random.uniform((pos_bw[1] + 0.25), (pos_tw[1] - 0.25)))
+        # check if it is placed on the robot
+        while rob.position()[0] + 0.25 > new_pos[0] > rob.position()[0] - 0.25 \
+                and rob.position()[1] + 0.25 > new_pos[1] > rob.position()[1] - 0.25:
+            new_pos[0] = (random.uniform((pos_lw[0] + 0.25), (pos_rw[0] - 0.25)))
+            new_pos[1] = (random.uniform((pos_bw[1] + 0.25), (pos_tw[1] - 0.25)))
+            #checks if the food doesn't overlap already placed food
+            for x in food_pos:
+                if new_pos == x:
+                    new_pos[0] = (random.uniform((pos_lw[0] + 0.25), (pos_rw[0] - 0.25)))
+                    new_pos[1] = (random.uniform((pos_bw[1] + 0.25), (pos_tw[1] - 0.25)))      
+                else:
+                    food_pos.append(new_pos)
+        vrep.simxSetObjectPosition(rob._clientID, food_handle, -1, new_pos, vrep.simx_opmode_blocking)
+
 
 def foraging(rob, actions):
     """look for food and eat it"""
 
     # init vars
+    episode_counter = 0
     time_passed = 0
     df = pd.DataFrame()
+    food_eaten = 0
+    food_names = ['Food', 'Food0', 'Food1', 'Food2', 'Food3', 'Food4', 'Food5']  # TODO: make this generic for arbitraty foods
 
     # loop forever until terminated manually
     while True:
@@ -189,21 +238,24 @@ def foraging(rob, actions):
         action = actions[action_index]
         l, r, t = action
         
-        # print actions (l_wheel speed | r_wheel speed | time moving (ms))
-        print(f'action: {action_index}:\t{l}\t{r}\t{t}')
         # save starting positionn (before move)
         start_pos = rob.position()
         # move robot
         rob.move(l, r, t)
+        food_eaten = rob.collected_food() - episode_counter * len(food_names)
         # save finishing position (after move)
         stop_pos = rob.position()
 
         distance = calc_distance_travelled(start_pos, stop_pos)
 
+
+        # print actions (l_wheel speed | r_wheel speed | time moving (ms))
+        print(f'action: {action_index}:\t{l}\t{r}\t{t}\t collected food: {food_eaten}')
+
         df = df.append({
             "action_index": action_index,
             "time_elapsed": time_passed,
-            # "episode_index": int(self.episode_counter),
+            "episode_index": int(episode_counter),
             # "observations:": self.observations,
             # "reward": overall_reward,
             # "object_in_range": object_in_range,
@@ -217,10 +269,20 @@ def foraging(rob, actions):
             }, ignore_index=True)
 
         time_passed += t
+
+        # stopping and resetting
+        done = False
+        if food_eaten == len(food_names):
+            done = True
+            food_eaten = 0
+            episode_counter += 1
+            _reset_food(rob, food_names)
+
         # if 5 minutes passed, print save dataframe and stop
-        if time_passed > 240000:
+        if time_passed > 240000 or done:
             df.to_csv(f'results/{info.task}/{info.user}/{info.scene}/dumb_robobo_progress.tsv', sep='\t')
-            break
+
+            # break
 
 def object_avoidance(rob, actions):
     """ obstacle avoidance hard-coded algorithm """
